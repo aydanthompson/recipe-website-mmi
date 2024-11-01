dea8a3461a49eae5ae3a68c98cd82df862f3c5d8
Aydan Thompson <aydan.thompson@bt.com>
2024-11-01 01:15:38 +0000

    Added error reporting, added whitespace for readability

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set("display_errors", "1");
+
 $page = $_GET["p"];
+
 if (!$page) {
   $page = "home";
 }
+
 require_once(__DIR__ . "/includes/header.php");
 require_once(__DIR__ . "/pages/" . $page . ".php");
 require_once(__DIR__ . "/includes/header.php");
